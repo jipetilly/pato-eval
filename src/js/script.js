0c30e13ab91e644dd63e15ca553e129a55c0f031
jipetilly <jipetilly@gmail.com>
2021-04-01 12:05:02 +0200

    Pato Eval J2 12h

--- src/js/script.js
+++ src/js/script.js
@@ -8,17 +8,50 @@ const swiper = new Swiper('.swiper-container', {
   },
   effect: 'fade',
   loop: true,
-  
-    // If we need pagination
+      // If we need pagination
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
     },
-  
-    // Navigation arrows
+      // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-
   });
+
+// PARALLAX 
+
+// $('.parallax-window').parallax({imageSrc: '/img/bg-intro-01.jpg'});
+
+
+// NAVIGATION
+
+window.addEventListener("scroll", function () {
+    var e = window.pageYOffset,
+        s = (document.getElementById("nav"), document.getElementById("header"));
+    console.log(e), e >= 100 ? s.classList.add("nav-js") : s.classList.remove("nav-js");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//navigation scroll-down
+let mainNav = document.querySelector('.main-nav');
+window.addEventListener('scroll', function(){
+    window.pageYOffset > mainNav.clientHeight ? mainNav.classList.add("scroll-down") : mainNav.classList.remove("scroll-down");
+});
