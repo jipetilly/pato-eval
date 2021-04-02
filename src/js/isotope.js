db80ffb2c1dbd09cf89897d848288ca7f61e1073
jipetilly <jipetilly@gmail.com>
2021-04-02 16:04:16 +0200

    Pato Eval END

--- src/js/isotope.js
+++ src/js/isotope.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
 
     $(".wrap-gallery").isotope({
-        itemSelector: '.item-gallery',
-
-        
+        itemSelector: '.picture-gallery',
     });
 
+// FILTER ISOTOPE LABEL
+    $(".wrap-label a").click(function(){
+        $('.wrap-label a').removeClass("label-active");
+        $(this).addClass("label-active");
+    
+        var selector = $(this).attr("data-filter");
+        $(".wrap-gallery").isotope({
+            filter: selector,
+        });
+        return false
+    });
 }); 
--- src/js/script.js
+++ src/js/script.js
@@ -56,3 +56,34 @@ closeBtn.addEventListener('click', function(){
     overlaySideMenu.classList.remove('show-overlay-sidemenu')
 });
 
+// MAGNIFIC POPUP VIDEO
+
+$(document).ready(function() {
+	$('.popup-youtube').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+});
+
+// MAGNIFIC POPUP GALLERY
+
+    $('.wrap-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1]
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+        }
+        
+    });
